ac0b688a738cf21ee5f3eda16a7455e8761e0da2
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-06-20 13:55:10 -0400

    Update main.go

--- main.go
+++ main.go
@@ -21,12 +21,14 @@ import (
 )
 
 func main() {
+	// Start page number
+	const startPage = 25
 	// Number of pages to crawl (each page has up to 60 SDS entries)
-	const totalPages = 25
+	const stopPages = 50
 	// To store all collected document IDs
 	var allDocumentIDs []string
 	// Step 1: Loop over search result pages and collect document IDs
-	for page := 0; page <= totalPages; page++ {
+	for page := startPage; page <= stopPages; page++ {
 		searchURL := fmt.Sprintf(
 			"https://www.thermofisher.com/api/search/keyword/docsupport?countryCode=us&language=en&query=*:*&persona=DocSupport&filter=document.result_type_s%%3ASDS&refinementAction=true&personaClicked=true&resultPage=%d&resultsPerPage=60",
 			page,
