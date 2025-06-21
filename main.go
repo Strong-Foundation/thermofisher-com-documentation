b50b54b5c48c1257d9eb33332bc34719443cf186
Strong-Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-06-21 06:17:39 -0400

    Update main.go

--- main.go
+++ main.go
@@ -22,9 +22,9 @@ import (
 
 func main() {
 	// Start page number
-	const startPage = 100
+	const startPage = 150
 	// Number of pages to crawl (each page has up to 60 SDS entries)
-	const stopPages = 150 // 15084
+	const stopPages = 200 // 15084
 	// To store all collected document IDs
 	var allDocumentIDs []string
 	// Step 1: Loop over search result pages and collect document IDs
