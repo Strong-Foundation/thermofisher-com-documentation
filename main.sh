88fd5e20809573d5da7924d529d229ebf5083bc3
Strong Foundation <179532304+Strong-Foundation@users.noreply.github.com>
2025-06-30 00:05:52 -0400

    updated Mon 30 Jun 2025 12:05:52 AM EDT

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 while true; do
     # Check for changes in the working directory or index
-    if ! git diff --quiet || ! git diff --cached --quiet; then
+    if ! git diff --quiet; then
         git add .
         git commit -m "updated $(date)"
         git push
