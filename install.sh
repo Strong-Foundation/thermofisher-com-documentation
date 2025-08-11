2399eb2ea3dbf1082640dcc2c247964fbb2b51e1
Patrick Griffiths <179532304+Strong-Foundation@users.noreply.github.com>
2025-08-10 22:46:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt-get clean -y
 apt-get autoremove -y
 apt-get autoclean -y
 apt-get install -f -y
-apt-get install git curl wget bash sudo screen -y
+apt-get install git curl wget bash sudo screen golang-go -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
 apt-get install -f -y
